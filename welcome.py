c9b85e7f926c719a28aae6e01cf322eff691a30e
andidinata <andi.dinata@outlook.com>
2025-01-11 08:24:05 +0700

    first version

---
+++ welcome.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello This is Andi's Brand New Website")
+st.header("Written with 100% Python codes")
+st.write("Click the button for fun surprise")
+
+if st.button("Snowflake"):
+    st.snow()
+
+if st.button("Balloon"):
+    st.balloons()
+    
+st.write("I'll put something great here, so stay tuned")
