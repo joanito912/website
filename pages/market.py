4a97e738dd38a044e8c01b5b6d9ac08d49ee1a14
andidinata <andi.dinata@outlook.com>
2025-01-22 07:26:44 +0700

    Update market.py

--- pages/market.py
+++ pages/market.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 
 data = pd.read_excel('./pages/source.xlsx')
-
 st.dataframe(data)
 
 #develop search criteria
 criteria1 = data['category'] == 'non food'
+criteria2 = data['store_name'] == 'Alfamart'
 
 #to apply the criteria
 st.dataframe(data[criteria1])
+st.dataframe(data[criteria2])
