29a75af278636f9161e35efebbd573a906515f2a
andidinata <andi.dinata@outlook.com>
2025-01-25 12:03:32 +0700

    Update blog.py

--- pages/blog.py
+++ pages/blog.py
@@ -3,7 +3,7 @@ import os
 
 st.title("My blog page")
 
-with open('./blog/article.md','r') as f:
+with open('./pages/blog/article.md','r') as f:
   content = f.read()
 f.close()
 st.markdown('content')
