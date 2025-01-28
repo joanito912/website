0464c07ed9e5347b1af5c371cba1a9b8cc5df45d
andidinata <andi.dinata@outlook.com>
2025-01-28 15:30:23 +0700

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.header("This website is written with 100% Python codes")
 st.write("Click the button for fun surprise")
 st.markdown("""
-            <style>
-                body {
-                    background-color: #00d4ff;
-                }
-            </style>
-            """, unsafe_allow_html=True)
+<style>
+    body {
+        background-color: #00d4ff;
+    }
+</style>
+""", unsafe_allow_html=True)
 
 if st.button("Snowflake"):
     st.snow()
