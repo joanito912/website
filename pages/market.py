77331f091ff71c720f6040aeca2774ddff5756ff
andidinata <andi.dinata@outlook.com>
2025-02-01 13:33:58 +0700

    Update market.py

--- pages/market.py
+++ pages/market.py
@@ -5,20 +5,24 @@ data = pd.read_excel('./pages/source.xlsx')
 st.dataframe(data)
 
 category_values = data['category'].unique()
-selected_category = st.selectbox("Select Category",options=category_values)
+selected_category = st.multiselect("Select Category",options=category_values)
 
-#develop search criteria
-criteria1 = data['category'] == selected_category
-criteria2 = data['store_name'] == 'Alfamart'
+store_values = data['store_name'].unique()
+selected_store = st.multiselect("Select Store",options=store_values)
+
+# develop search criteria
+criteria1 = data['category'].isin(selected_category)
+criteria2 = data['store_name'].isin(selected_store)
 criteria3 = (criteria1) & (criteria2)
+
 criteria4 = data['price'] >= 10000
 criteria5 = data['price'] <= 30000
 criteria6 = (criteria4) & (criteria5)
 
 #to apply the criteria
-st.dataframe(data[criteria1])
-st.dataframe(data[criteria2])
+# st.dataframe(data[criteria1])
+# st.dataframe(data[criteria2])
 st.dataframe(data[criteria3])
-st.dataframe(data[criteria4])
-st.dataframe(data[criteria5])
-st.dataframe(data[criteria6])
+# st.dataframe(data[criteria4])
+# st.dataframe(data[criteria5])
+# st.dataframe(data[criteria6])
