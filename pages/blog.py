cf0c7f4a6e7fb5965ac97d731e10f394ff5b118e
andidinata <andi.dinata@outlook.com>
2025-02-01 11:51:21 +0700

    Update blog.py

--- pages/blog.py
+++ pages/blog.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import os
 
-st.title("My blog page")
-
-with open('./pages/blog/article.md','r') as f:
+with open('./pages/blog/school_agenda.md','r') as f:
   content = f.read()
 f.close()
 st.markdown(content)
