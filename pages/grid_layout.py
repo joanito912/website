bfe743b5d1af1f58e4dfa75a9858b6081efc08a1
andidinata <andi.dinata@outlook.com>
2025-02-16 05:25:50 +0700

    update

--- pages/grid_layout.py
+++ pages/grid_layout.py
@@ -1,22 +1,28 @@
 import streamlit as st
 st.set_page_config(layout='wide')
 
-st.markdown('### Two methods of grid you may choose, the first method on left, and second on the right column.')
+st.markdown('### Simple Grid Method')
 
 col1,col2 = st.columns(2)
 
 with col1:
-    
-    total_cols = st.number_input("Columns",value=3,step=1)
-    total_rows = st.number_input("Rows",value=3,step=1)
+    st.code('''
 
-    for nr in range(total_rows):
-        with st.container(border=True):
-            columns = st.columns(total_cols)
-            for nc, column in enumerate(columns):
-                with column:
-                    with st.container(border=True):
-                        st.write(f"container {nr}, column {nc}")
+            data_length = st.number_input("Data Length",value=16, step=1)
+            total_column = st.number_input("Total Columns",value=3,step=1)
+
+            columns = st.columns(total_column)
+
+            for i in range(data_length):
+                for r in range(total_column):
+                    if i%total_column == r:
+                        column = columns[r]
+                    
+                        with column:
+                            with st.container(border=True):
+                                st.write(i))
+            '''
+
 
 with col2:
     data_length = st.number_input("Data Length",value=16, step=1)
@@ -31,6 +37,4 @@ with col2:
             
                 with column:
                     with st.container(border=True):
-                        st.write(f"Data {i+1}")
-            
-        
+                        st.write(i)
