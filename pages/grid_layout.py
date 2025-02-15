44cf6795a01c1b67d03530a02397032d198cad54
andidinata <andi.dinata@outlook.com>
2025-02-16 05:12:30 +0700

    update

--- pages/grid_layout.py
+++ pages/grid_layout.py
@@ -1,12 +1,33 @@
 import streamlit as st
 
-total_cols = st.number_input("Columns",step=1)
-total_rows = st.number_input("Rows",step=1)
+st.markdown('### Two methods of grid you may choose, the first method on left, and second on the right column.')
 
-for nr in range(total_rows):
-    with st.container(border=True):
-        columns = st.columns(total_cols)
-        for nc, column in enumerate(columns):
+col1,col2 = st.columns(2)
+
+with col1:
+    total_cols = st.number_input("Columns",value=3,step=1)
+    total_rows = st.number_input("Rows",value=3,step=1)
+
+    for nr in range(total_rows):
+        with st.container(border=True):
+            columns = st.columns(total_cols)
+            for nc, column in enumerate(columns):
+                with column:
+                    with st.container(border=True):
+                        st.write(f"container {nr}, column {nc}")
+
+with col2:
+    data_length = st.number_input("Columns",value=16, step=1)
+    total_columns = 4
+
+    columns = st.columns(total_columns)
+
+    for i in range(total_columns):
+        if i%total_columns == 0:
+            column = columns[i]
+            
             with column:
                 with st.container(border=True):
-                    st.write(f"container {nr}, column {nc}")
+                    st.write("Hello")
+            
+        
