6f531ec5c93316ce8229e09950f5cddb46cb9ab7
andidinata <andi.dinata@outlook.com>
2025-02-22 11:29:55 +0700

    Update Matteo_Blog.py

--- pages/Matteo_Blog.py
+++ pages/Matteo_Blog.py
@@ -4,7 +4,8 @@ import os
 files = os.listdir('./pages/blog')
 
 for each_file in files:
-    with open('./pages/blog/' + each_file,'r') as f:
-      content = f.read()
-    f.close()
-    st.markdown(content)
+    with st.container(border=True):
+        with open('./pages/blog/' + each_file,'r') as f:
+          content = f.read()
+        f.close()
+        st.markdown(content)
