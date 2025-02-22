734ce3d0ceb0238bc724b234156a74b50caee35a
andidinata <andi.dinata@outlook.com>
2025-02-22 12:01:12 +0700

    Update grid layout.py

--- pages/grid layout.py
+++ pages/grid layout.py
@@ -6,13 +6,13 @@ columns = st.columns(ncolumn)
 
 for i in range(ncolumn):
     if i%ncolumn == 0:
-        col = column[0]
+        col = columns[0]
     if i%ncolumn == 1:
-        col = column[1]
+        col = columns[1]
     if i%ncolumn == 2:
-        col = column[2]  
+        col = columns[2]  
     if i%ncolumn == 3:
-        col = column[3]
+        col = columns[3]
 
     with col:
         with st.container(border=True):
