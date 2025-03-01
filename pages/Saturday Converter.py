14dfa2378671def97b8c40d6e7efc01659a497cb
andidinata <andi.dinata@outlook.com>
2025-03-01 17:49:21 +0700

    Update Saturday Converter.py

--- pages/Saturday Converter.py
+++ pages/Saturday Converter.py
@@ -1,21 +1,48 @@
 import streamlit as st
+st.set_page_config(layout='wide')
+
+st.title('Fun Conversion App')
 
 conversion_factors = {
   'distance':{'mm':1,
               'cm':0.1,
               'm' :0.01},
-  'calories': {'fries':319,
-               'donut':190,
-               'burger':290,
-               'ice cream':207,
-               'apple':52.1,
-               'banana':88.7,
-               'cabbage':24.6,
-               'coke':139,
-               'milk':42.3},
-  'weight':None,
-  'time':None
-  }
+  'calories': {'fries':319/319,
+               'donut':319/190,
+               'burger':319/290,
+               'ice cream':319/207,
+               'apple':319/52.1,
+               'banana':319/88.7,
+               'cabbage':319/24.6,
+               'coke':319/139,
+               'milk':319/42.3,
+               'instant noodles':319/340},
+  'weight':{'gr':1/0.001,
+            'kg':1,
+            'ton':1/1000,
+            'adult person':1/70,
+            'a teenager':1/40,
+            'cat':None,
+            'mouse':None,
+            'dog':None,
+            'cow':None,
+            'elephant':None,
+            'giraffe':None,
+            'penguin':None,
+            'dolphin':None,
+            'blue whale':None,
+            'brontosaurus':None,
+            't-rex':None},
+  'time':{'seconds':1/(1/3600),
+          'minutes':1/(1/60),
+          'hour':1,
+          'day':1/24,
+          'week':1/(24*7),
+          'month':1/(24*7*4),
+          'year':1/(24*7*52),
+          'cat year':1/(24*7*52)*7,
+          'dog year':None}}
+
 col1,col2,col3,col4,col5 = st.columns(5)
 
 with col1:
@@ -24,15 +51,19 @@ with col1:
     category = st.radio("Select category",options=category_list)
 
 with col2:
-    input_number = st.number_input("Input",min_value=1,value=1)
+    input_value = st.number_input("Input",min_value=1,value=1)
     
 with col3:
     base_unit_list = list(conversion_factors[category].keys())
     base_unit = st.radio("From:",options=base_unit_list)
+    base_cf = conversion_factors[category][base_unit]
 
 with col4:
     target_unit_list = list(conversion_factors[category].keys())
     target_unit = st.radio("To:",options=target_unit_list)
+    target_cf = conversion_factors[category][target_unit]
 
 with col5:
-    
+    st.write("Output:")
+    output_value = input_value / base_cf * target_cf
+    st.write(f'The {category} of {input_value} {base_unit} equals to {output_value:.2f} {target_unit}')
