4226733f5579055edfa034cdd81cc71832d30c55
andidinata <andi.dinata@outlook.com>
2025-03-08 12:02:57 +0700

    Update Matteo_Blog.py

--- pages/Matteo_Blog.py
+++ pages/Matteo_Blog.py
@@ -2,9 +2,9 @@ import streamlit as st
 import os
 
 files = os.listdir('./pages/blog')
+st.write(files)
 
-
-for each_file in files.sort():
+for each_file in files:
     with st.container(border=True):
         with open('./pages/blog/' + each_file,'r') as f:
           content = f.read()
