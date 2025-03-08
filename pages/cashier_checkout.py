0fce9a2251a03f7bed6cea13de35dca0d721672d
andidinata <andi.dinata@outlook.com>
2025-03-08 13:36:49 +0700

    Update cashier_checkout.py

--- pages/cashier_checkout.py
+++ pages/cashier_checkout.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout='wide')
+
+dummy_data = {'description':['blueband','bread','egg'],
+              'price':[12000,18000,15000]}
 
 leftcolumn,rightcolumn = st.columns(2)
 
@@ -8,6 +12,8 @@ with leftcolumn:
     pass
     with st.container(border=True):
         st.write("Check-out items:")
+        st.dataframe(dummy_data)
+        
 
     with st.container(border=True):
         st.write("Total:")
