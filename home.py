251f123f7cb6b69a4eac3324a35e44461a6c6d03
andidinata <andi.dinata@outlook.com>
2025-03-29 18:00:30 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,18 +3,29 @@ import streamlit as st
 st.info("Welcome to Colab Coding WebApp, choose the app on the side bar. Hope you find it useful. Uploaded data is not stored in github repository")
 
 pages = {
-    "Projects" : [
+    "Demo" : [
         st.Page("./pages/01_Unit Converter.py",title = "Converter",icon="📐"),
         st.Page("./pages/02_Online Marketplace.py",title = "Online Store",icon=":material/local_mall:"),
         st.Page("./pages/03_Cashier Webapp.py",title = "Cashier",icon=":material/payments:"),
-        st.Page("./pages/04_Triangle Calculator.py",title = "Triangle",icon=":material/change_history:")
+        st.Page("./pages/04_Triangle Calculator.py",title = "Triangle",icon=":material/change_history:"),
+        st.Page("./pages/Camera demo.py",title = "Camera",icon=":material/photo_camera:")
     ],
     "Tools" : [
-        st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF"),
-        st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation"),
-        st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation"),
-        st.Page("./pages/09_Led Matrix Simulation.py",title="Led Matrix Simulation")
-    ]
+        st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF",icon=":material/picture_as_pdf:"),
+        st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation",icon=":material/inventory:"),
+        st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation",icon=":material/inventory_2:"),
+        st.Page("./pages/09_Led Matrix Simulation.py",title="Led Matrix Simulation",icon=":material/apps:")
+    ],
+    "Class Project" : [
+        st.Page("./pages/Fun Converter.py",title = "Fun Converter",icon=":material/scale:"),
+        st.Page("./pages/Haadi Online Store.py",title = "Haadi Marketplace",icon=":material/local_mall:"),
+        st.Page("./pages/Jovano Marketplace.py",title = "Jovano Marketplace",icon=":material/local_mall:"),
+        st.Page("./pages/Saturday Marketplace.py",title = "Saturday Marketplace",icon=":material/local_mall:"),
+        st.Page("./pages/Matteo_Blog.py",title = "Matteo Blog",icon=":material/rss_feed:"),
+        st.Page("./pages/photo gallery.py",title = "Photo Gallery",icon=":material/photo_library:")
+        
+    ],
+
 }
 
 pg = st.navigation(pages)
