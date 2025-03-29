10fdec20133d630d245bdd15c17a31f1d75cfee8
andidinata <andi.dinata@outlook.com>
2025-03-29 13:31:11 +0700

    Update home.py

--- home.py
+++ home.py
@@ -8,15 +8,15 @@ st.write("Security: uploaded data is not stored in github repository")
 
 pages = {
     "Projects" : [
-        st.Page("01_Unit Converter.py",title = "Converter"),
-        st.Page("02_Online Marketplace.py",title = "Online Store"),
-        st.Page("03_Cashier Webapp.py",title = "Cashier"),
-        st.Page("04_Triangle Calculator.py",title = "Triangle")
+        st.Page("./pages/01_Unit Converter.py",title = "Converter"),
+        st.Page("./pages/02_Online Marketplace.py",title = "Online Store"),
+        st.Page("./pages/03_Cashier Webapp.py",title = "Cashier"),
+        st.Page("./pages/04_Triangle Calculator.py",title = "Triangle")
     ],
     "Tools" : [
-        st.Page("05_PDF Rotator.py",title="Rotate PDF"),
-        st.Page("06_Inventory Simulation.py",title="Inventory Simulation"),
-        st.Page("07_Batch Inventory Simulation.py",title="Batch Inventory Simulation"),
-        st.Page("09_Led Matrix Simulation.py",title="Led Matrix Simulation")
+        st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF"),
+        st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation"),
+        st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation"),
+        st.Page("./pages/09_Led Matrix Simulation.py",title="Led Matrix Simulation")
     ]
 }
