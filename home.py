a552d9525d8d7f1904f3e5603c29b7d335c69704
andidinata <andi.dinata@outlook.com>
2025-04-16 16:09:09 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.info("Welcome to Colab Coding WebApp, choose the app on the side bar. Hope you find it useful. Uploaded data is not stored in github repository")
 
 pages = {
-    "Demo" : [
-        st.Page("./pages/01_Unit Converter.py",title = "Converter",icon="📐"),
-        st.Page("./pages/02_Online Marketplace.py",title = "Online Store",icon=":material/local_mall:"),
-        st.Page("./pages/03_Cashier Webapp.py",title = "Cashier",icon=":material/payments:"),
-        st.Page("./pages/04_Triangle Calculator.py",title = "Triangle",icon=":material/change_history:"),
-        st.Page("./pages/Camera demo.py",title = "Camera",icon=":material/photo_camera:")
-    ],
+    # "Demo" : [
+    #     st.Page("./pages/01_Unit Converter.py",title = "Converter",icon="📐"),
+    #     st.Page("./pages/02_Online Marketplace.py",title = "Online Store",icon=":material/local_mall:"),
+    #     st.Page("./pages/03_Cashier Webapp.py",title = "Cashier",icon=":material/payments:"),
+    #     st.Page("./pages/04_Triangle Calculator.py",title = "Triangle",icon=":material/change_history:"),
+    #     st.Page("./pages/Camera demo.py",title = "Camera",icon=":material/photo_camera:")
+    # ],
     "Tools" : [
         st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF",icon=":material/picture_as_pdf:"),
         st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation",icon=":material/inventory:"),
