77b065588e7845b7d6101fe3d229e18ded90e33c
andidinata <andi.dinata@outlook.com>
2025-04-16 11:25:31 +0700

    Update home.py

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ pages = {
     "Tools" : [
         st.Page("./pages/05_PDF Rotator.py",title="Rotate PDF",icon=":material/picture_as_pdf:"),
         st.Page("./pages/06_Inventory Simulation.py",title="Inventory Simulation",icon=":material/inventory:"),
-        st.Page("./pages/06B_Inventory Simulation v2.py",title="Inventory Simulation",icon=":material/inventory:"),
+        st.Page("./pages/06B_Inventory Simulation v2.py",title="Inventory Simulation V2",icon=":material/inventory:"),
         st.Page("./pages/07_Batch Inventory Simulation.py",title="Batch Inventory Simulation",icon=":material/inventory_2:"),
         st.Page("./pages/Calculator.py",title="Led Matrix Simulation",icon=":material/apps:")
     ],
